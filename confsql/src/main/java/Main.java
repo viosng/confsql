945819bc7df998adc60a4ccd30d6d42269192044
Nickolay <Nickolay.Saveliev@emc.com>
2014-12-19 14:41:47 +0300

    init commit

---
+++ confsql/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: vio
+ * Date: 19.12.2014
+ * Time: 15:41
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
